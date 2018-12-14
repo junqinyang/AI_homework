5a6969f6a208b51df3af047c07569433660abcfb
vichubtor <1533508869@qq.com>
2018-12-14 07:49:02 -0500

    20:48 14/12 from gdm1

--- TF/mnist_training.py
+++ TF/mnist_training.py
@@ -4,8 +4,13 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
+'''
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
-
+acc = np.array([])
+'''
 mnist = input_data.read_data_sets('./',one_hot=True)
 
 def add_layer(inputs,in_size,out_size,n_layer,activation_function=None):
@@ -48,7 +53,7 @@ with tf.name_scope('cross_entropy'):
     cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),reduction_indices=[1]))
     tf.summary.scalar('cross_entropy',cross_entropy)
 
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.4).minimize(cross_entropy)
 
 sess = tf.Session()
 
@@ -63,6 +68,10 @@ for i in range(1000):
     sess.run(train_step,feed_dict={xs:batch_xs,ys:batch_ys})
 
     if i % 50 == 0:
+        result = sess.run(merged,feed_dict={xs:batch_xs,ys:batch_ys})
+        writer.add_summary(result,i)
         print(compute_accuracy(mnist.test.images,mnist.test.labels))
+        
+
 
 
